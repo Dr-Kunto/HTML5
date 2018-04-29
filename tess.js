cb56a984ac4cd0ee193c016200664079fa2fa60f
Кцйто Сцсц Дюдм <desa.beringin@gmail.com>
2018-04-29 20:16:15 +0700

    Add files via upload

---
+++ tess.js
@@ -0,0 +1,5 @@
+var player = videojs('player', {});
+
+player.on(['loadstart', 'play', 'playing', 'firstplay', 'pause', 'ended', 'adplay', 'adplaying', 'adfirstplay', 'adpause', 'adended', 'contentplay', 'contentplaying', 'contentfirstplay', 'contentpause', 'contentended', 'contentupdate'], function(e) {
+  console.warn('VIDEOJS player event: ',  e.type);
+});
