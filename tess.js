5ea291e3ff145697211edf30f40947d6de546d50
Кцйто Сцсц Дюдм <desa.beringin@gmail.com>
2018-05-14 18:21:58 +0700

    Delete tess.js

--- tess.js
+++
@@ -1,5 +0,0 @@
-var player = videojs('player', {});
-
-player.on(['loadstart', 'play', 'playing', 'firstplay', 'pause', 'ended', 'adplay', 'adplaying', 'adfirstplay', 'adpause', 'adended', 'contentplay', 'contentplaying', 'contentfirstplay', 'contentpause', 'contentended', 'contentupdate'], function(e) {
-  console.warn('VIDEOJS player event: ',  e.type);
-});
